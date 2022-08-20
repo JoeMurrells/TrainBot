115af5c2c47e1ace70728deefe2546880e3aa6d6
Joe <josephmurrells@gmail.com>
2022-08-20 16:01:07 +0100

    Created search class allowing used to search for a given station

--- TrainBot/Commands.cs
+++ TrainBot/Commands.cs
@@ -16,7 +16,6 @@ namespace TrainBot
         public async Task Train(string stationOne, string stationTwo, string time = "")
         {
             
-
             if(TrainStation.Valid(stationOne,stationTwo) == true)
             {
                 await ReplyAsync(BotResponse.OutputString(stationOne, stationTwo, time));
@@ -26,14 +25,26 @@ namespace TrainBot
                 await ReplyAsync("Invalid station entered");
             }
 
-            
-
         }
 
         [Command("help")]
         public async Task Help()
         {
-            await ReplyAsync("Enter the command !train followed by the departing station and destination station using the stations CRS code" + "\n" + "Example \"!train VIC BTN\"" + "\n" + "" + "\n" + "You can optionally enter a time in HHMM format to show trains departing 2 hours either side of the given time" + "\n" + "Example \"!train VIC BTN 1630\"");
+            await ReplyAsync("Enter the command !train followed by the departing station and destination station using the stations CRS code" + "\n" + "Example \"!train VIC BTN\"" + "\n" + "" + "\n" + "You can optionally enter a time in HHMM format to show trains departing 2 hours either side of the given time" + "\n" + "Example \"!train VIC BTN 1630\"" + "\n" + "Type !search followed by the station name or 3 digit CRS code to get list of matching stations.");
+        }
+
+        [Command("search")]
+        public async Task SearchForStation(string searchStation)
+        {
+            if(searchStation.Length < 3)
+            {
+                await ReplyAsync("Station name or CRS code must be more than 3 characters!");
+            }
+            else
+            {
+                await ReplyAsync(Search.SearchStation(searchStation));
+            }
+            
         }
 
     }
---
+++ TrainBot/Search.cs
@@ -0,0 +1,34 @@
+﻿using FuzzySharp;
+using System.Collections.Generic;
+
+
+namespace TrainBot
+{
+    public static class Search
+    {
+        public static string SearchStation(string searchStation)
+        {
+            string outputString = "";
+
+            foreach(KeyValuePair<string, string> station in TrainStation.Stations)
+            {
+                var textMatch = Fuzz.WeightedRatio(searchStation.ToLower(), station.Key.ToLower());
+                var codeMatch = Fuzz.WeightedRatio(searchStation.ToLower(), station.Value.ToLower());
+
+                if (textMatch > 85 || codeMatch > 85)
+                {
+                    outputString += $"**{station.Key}**: {station.Value}" + "\n";
+                }
+            }
+
+            if(outputString == "")
+            {
+                return "No stations found :(";
+            }
+            else
+            {
+                return outputString;
+            }
+        }
+    }
+}
